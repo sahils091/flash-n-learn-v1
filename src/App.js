12b75b7a93bf8be980b31488e3330a570a088df5
Sahil Singh <sahils091@gmail.com>
2020-11-05 16:26:10 -0500

    Made axios call to api for dummy data, created a flash card, styled the flash card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import FlashcardList from './FlashcardList';
+import axios from "axios";
 
 function App() {
   const [flashCards, setFlashCards] = useState(SAMPLE_FLASHCARDS);
+
+
+  useEffect(()=>{
+    axios.get("https://opentdb.com/api.php?amount=10")
+    .then(res=> {
+     setFlashCards(res.data.results.map((questionItem, index) => {
+       const answer = decodeString(questionItem.correct_answer);
+       const options = [...questionItem.incorrect_answers.map(a => decodeString(a)),
+         answer]
+       return {
+         id: `${index} -${Date.now}`,
+         question: decodeString(questionItem.question),
+         answer: answer,
+         options:options.sort(() => Math.random() - 0.5)
+       }
+     })
+    )})
+  },[])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str
+    return textArea.value
+  }
   return (
-    <div className="App">
+    <div className="container">
     <FlashcardList flashCards={flashCards}/>  
     </div>
   );
--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const Flashcard = ({flashcard}) => {
+    const [flip, setFlip] = useState(false);
+    const [height, setHeight] = useState('initial');
+    const frontEl = useRef();
+    const backEl = useRef();
+
+function setMaxheight() {
+   const frontHeight = frontEl.current.getBoundingClientRect().height;
+   const backHeight = backEl.current.getBoundingClientRect().height;
+   setHeight(Math.max(frontHeight, backHeight, 100))
+}
+
+useEffect(setMaxheight, [flashcard.question,flashcard.answer, flashcard.options])
     return (
-        <div>
-        {flashcard.question}
+        <div 
+        style={{height: height}}
+        className={`card ${ flip ? "flip" : ""}`}
+        onClick={()=> setFlip(!flip)}>
+
+        <div className="front" ref={frontEl}>
+            {flashcard.question}
+            <div className="flashcard.options">
+                {flashcard.options.map(option => {
+                    return <div className="flashcard-option">{option}</div>
+                })}
+            </div>
+        </div>
+        <div className="back" ref={backEl}>
+            {flashcard.answer}
+        </div>
             
         </div>
     );
